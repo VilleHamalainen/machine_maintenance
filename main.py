f60342c6ee7b2129366f3bdd422195681153433b
Ville Hämäläinen <vilbohamalainen@gmail.com>
2024-02-08 12:38:36 +0200

    add forms

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
 import os
 
 app = Flask(__name__)
@@ -7,6 +7,25 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+
+@app.route('/student')
+def student_form():
+    # implement login?
+    return render_template('studentform.html')
+
+@app.route('/admin')
+def admin_form():
+    # implement login
+    return render_template('adminform.html')
+
+
+@app.route('/submit', methods = ['POST'])
+def submit():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    return f"Hello {name}, your email is {email}."
+
 
 
 if __name__ == '__main__':
