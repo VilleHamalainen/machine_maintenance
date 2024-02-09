26100d68e1f655600e087ddd555a1255e10fd8ae
Ville Hämäläinen <vilbohamalainen@gmail.com>
2024-02-09 14:14:01 +0200

    flask blog app demo

--- models/maintenance_repository.py
+++ models/maintenance_repository.py
@@ -1,8 +1,17 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, current_app, g
 from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+import click
 
 db = SQLAlchemy()
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(current_app.config['DATABASE',], detect_types= sqlite3.PARSE_DECLTYPES)
+        g.db.row_factory = sqlite3.Row
+    
+    return g.db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
